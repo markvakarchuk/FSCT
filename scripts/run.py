87c59696ace6e08ffc5f1a32fd97bcd3ce7cc038
markvakarchuk <44476510+markvakarchuk@users.noreply.github.com>
2024-04-27 17:50:29 -0400

    Basemap_SLAM_processing

--- scripts/run.py
+++ scripts/run.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
             num_cpu_cores=0,  # Number of CPU cores you want to use. If you run out of RAM, lower this. 0 means ALL cores.
             use_CPU_only=False,  # Set to True if you do not have an Nvidia GPU, or if you don't have enough vRAM.
             # Optional settings - Generally leave as they are.
-            slice_thickness=0.2,  # If your point cloud resolution is a bit low (and only if the stem segmentation is still reasonably accurate), try increasing this to 0.2.
+            slice_thickness=0.15,  # If your point cloud resolution is a bit low (and only if the stem segmentation is still reasonably accurate), try increasing this to 0.2.
             # If your point cloud is really dense, you may get away with 0.1. Default = 0.15
             slice_increment=0.05,  # The smaller this is, the better your results will be, however, this increases the run time.
             sort_stems=1,  # If you don't need the sorted stem points, turning this off speeds things up.
@@ -38,14 +38,14 @@ if __name__ == "__main__":
             tree_base_cutoff_height=5,  # A tree must have a cylinder measurement below this height above the DTM to be kept. This filters unsorted branches from being called individual trees.
             generate_output_point_cloud=1,  # Turn on if you would like a semantic and instance segmented point cloud. This mode will override the "sort_stems" setting if on.
             # If you activate "tree aware plot cropping mode", this function will use it.
-            ground_veg_cutoff_height=3,  # Any vegetation points below this height are considered to be understory and are not assigned to individual trees.
+            ground_veg_cutoff_height=4,  # Any vegetation points below this height are considered to be understory and are not assigned to individual trees.
             veg_sorting_range=1.5,  # Vegetation points can be, at most, this far away from a cylinder horizontally to be matched to a particular tree.
             stem_sorting_range=1,  # Stem points can be, at most, this far away from a cylinder in 3D to be matched to a particular tree.
             taper_measurement_height_min=0,  # Lowest height to measure diameter for taper output.
             taper_measurement_height_max=30,  # Highest height to measure diameter for taper output.
             taper_measurement_height_increment=0.2,  # diameter measurement increment.
             taper_slice_thickness=0.4,  # Cylinder measurements within +/- 0.5*taper_slice_thickness are used for taper measurement at a given height. The largest diameter is used.
-            delete_working_directory=True,  # Generally leave this on. Deletes the files used for segmentation after segmentation is finished.
+            delete_working_directory=False,  # Generally leave this on. Deletes the files used for segmentation after segmentation is finished.
             # You may wish to turn it off if you want to re-run/modify the segmentation code so you don't need to run pre-processing every time.
             minimise_output_size_mode=0,  # Will delete a number of non-essential outputs to reduce storage use.
         )
